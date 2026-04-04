b48f7fdc8b20f92ef6a1e571cbcdaf9966556472
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-04-03 21:07:13 -0500

    Added interface for the game entries

---
+++ src/api/v1/models/gameModel.ts
@@ -0,0 +1,10 @@
+export interface Game {
+  id: string;
+  title: string;
+  genre: string;
+  platform: string;
+  franchise?: string;
+  releaseYear?: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
