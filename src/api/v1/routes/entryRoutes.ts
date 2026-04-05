855f037bccae3c9861abe99e7eca9a1a5b696432
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-04-05 17:31:07 -0500

    Implemented joi validation into entries as well, works as expected

--- src/api/v1/routes/entryRoutes.ts
+++ src/api/v1/routes/entryRoutes.ts
@@ -1,15 +1,15 @@
 import express, { Router } from "express";
 import * as entryController from "../controllers/entryController";
 //import authenticate from "../middleware/authenticate";
-//import { validateRequest } from "../middleware/validate";
-//import { entrySchemas } from "../validations/entryValidation";
+import { validateRequest } from "../middleware/validate";
+import { entrySchemas } from "../validation/entrySchemas";
 
 const router: Router = express.Router();
 
-router.get("/", entryController.getAllEntries);
-router.get("/:id", entryController.getEntryById);
-router.post("/", entryController.createEntry);
-router.put("/:id", entryController.updateEntry);
-router.delete("/:id", entryController.deleteEntry);
+router.get("/", validateRequest(entrySchemas.create), entryController.getAllEntries);
+router.get("/:id", validateRequest(entrySchemas.getById), entryController.getEntryById);
+router.post("/", validateRequest(entrySchemas.create), entryController.createEntry);
+router.put("/:id", validateRequest(entrySchemas.update), entryController.updateEntry);
+router.delete("/:id", validateRequest(entrySchemas.delete), entryController.deleteEntry);
 
 export default router;
