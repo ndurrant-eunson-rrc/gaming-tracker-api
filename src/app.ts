d486baad502cb7bab3852f0c4407ea78e2202088
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-04-04 20:23:30 -0500

    Added swagger

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Express } from "express";
-
+import setupSwagger from "../config/swagger";
 import helmet from "helmet";
 import cors from "cors";
 
 const app: Express = express();
 
+setupSwagger(app);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
