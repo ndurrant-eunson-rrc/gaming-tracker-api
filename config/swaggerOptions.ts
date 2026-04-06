b21ccd80c353fc626e86bfc724582f1f14b53619
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-04-05 20:44:14 -0500

    Troubleshooting swagger, had a typo

--- config/swaggerOptions.ts
+++ config/swaggerOptions.ts
@@ -25,7 +25,7 @@ const swaggerOptions: swaggerJsdoc.Options = {
     },
     security: [{ bearerAuth: [] }],
   },
-  apis: ["./src/api/v1/routes/*.ts", "./src/api/v1/validations/*.ts"], // Path to the API docs and schemas
+  apis: ["./src/api/v1/routes/*.ts", "./src/api/v1/validation/*.ts"], // Path to the API docs and schemas
 };
 
 // Generate the Swagger spec
