8086b4a4e1d4032c3cec962a962b06f892b3f48d
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-04-09 12:35:51 -0500

    Adjusted the wishlist model

--- src/api/v1/models/wishlistModel.ts
+++ src/api/v1/models/wishlistModel.ts
@@ -1,16 +1,16 @@
 /**
  * Valid wishlist priorities
  */
-export type EntryStatus = "low" | "medium" | "high";
+export type WishlistPriority = "low" | "medium" | "high";
 
 /**
  * Represents a user's wishlist item for a game
  */
 export interface Wishlist {
   id: string; // firestore id
-  userId: string; // user id
+  userId: string; // id of the user who owns this wishlist item
   gameTitle: string; // game title
-  priority: EntryStatus; // user decides the priority
+  priority: WishlistPriority; // user decides the priority
   notes?: string; // any notes the user wants to include
   addedAt: Date | String; // when the game was added to the wishlist
   updatedAt: Date | String; // when the wishlist item was last updated
